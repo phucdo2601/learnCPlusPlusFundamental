ea64b0f5d4b5609ccddcb1c1d08f00b6ec4da666
Phuc Do Ngoc <ngocphucdo2601@gmail.com>
2024-05-19 18:02:35 +0700

    [learn-list-pro-fun-b01] 05. Basic learn typedef in c++ b01

---
+++ les05-typeof/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+
+// typedef std::vector<std::pair<std::string, int>> pairlist_t;
+// typedef std::string text_t;
+
+using text_t = std::string;
+
+namespace les04
+{
+    int main()
+    {
+        text_t fullName = "Phuc Do Ngoc";
+
+        std::cout << fullName << std::endl;
+
+        return 0;
+    }
+}
