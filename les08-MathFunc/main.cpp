d63d3f954844f4e333bc015638e741d92fdfea8e
Phuc Do Ngoc <ngocphucdo2601@gmail.com>
2024-05-20 21:52:25 +0700

    [learn-list-pro-fun-b01] 08. Basic learn math dunc b01

---
+++ les08-MathFunc/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+    double x = 3;
+    double y = 4;
+    double z;
+
+    z = pow(2, 4);
+    std::cout << z << std::endl;
+
+    return 0;
+}
