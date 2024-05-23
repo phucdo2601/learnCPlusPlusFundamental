a5382d74aa186e5c0c32a3518f4eb01edad1b2f8
Phuc Do Ngoc <ngocphucdo2601@gmail.com>
2024-05-23 15:16:05 +0700

    [learn-list-pro-fun-b01] 17. Basic learn while loop in c++ b01

---
+++ les17-whileLoop/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+    std::string name;
+
+    while (name.empty()){
+        std::cout << "Enter your name: " << std::endl;
+
+        std::getline(std::cin, name);
+    }
+    
+    
+    std::cout << "Hello " << name << std::endl;
+
+    return 0;
+}
